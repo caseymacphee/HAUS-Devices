c7278e8327cf02d7e29413e8057dc37fa55cb954
CharlesGust <charlesg@equality.net>
2014-11-25 21:04:12 -0800

    use contextlib for locks, support ubuntu guest serial ports, predictive field and key pair delimiters

--- devices.py
+++ devices.py
@@ -10,6 +10,7 @@ import json
 import threading
 import serial
 from threading import Lock
+from contextlib import closing
 
 
 class User(object):
@@ -59,80 +60,101 @@ The connection returns in it's open state .
                 connection.close()
             serial_list.append(connection)
         self.serial_connections = serial_list
-        return serial_list 
+        return serial_list
 
     def test_ports(self):
         pass
 
+    # CMGTODO: _ensure_port_is_open should be static function, or in another class
+    def _ensure_port_is_open(self, port):
+        if not port.isOpen():
+            port.open()
+        return
+
     def read_monitors_to_json(self, timeout = 30):
         ### listening for 30 second timeout for testing ###
         ### if you think your monitors are running slow, check for delays in your arduino sketch ###
-        start = time.time()
-        current_time = start
+        start_time = time.time()
 
-        while current_time - start < timeout:
+        while time.time() - start_time < timeout:
             for name, port in self.monitors.iteritems():
                 port_lock = self.device_locks[name]
+                # CMGTODO
+                # if we can't get a port_lock, it would probably be more
+                #  efficient to give up on the current monitor than have
+                #  everything else wait for it to become free.
+                with port_lock:
+                    self._ensure_port_is_open(port)
+                    # current = port.read()
+                    # while current is not '$':
+                    #     current = port.read()
+                    ## Your logic goes here ###
+                    message = port.readline()
+                    print message
+                    jsonmessage = self._build_json(message, name)
+                    print jsonmessage
 
-                # if port_lock.locked:
-                #     print 'locked'
-                port_lock.acquire()
-                if port_lock.locked():
-                    print port_lock,' acquired'
-                if not port.isOpen():
-                    port.open()
-                current = port.read()
-                while current is not '$':
-                    current = port.read()
-                ## Your logic goes here ###
-                message = port.readline()
-                jsonmessage = self._build_json(message, name)
-                print jsonmessage
-                port.close()
-                port_lock.release()
-                if not port_lock.locked():
-                    print port_lock, 'released'
-                last_time = current_time
-                current_time = time.time()
-                print name,' took: ', int(current_time - last_time), 'seconds'
+        print name,' took: ', int(time.time() - start_time), 'seconds'
 
     def talk_to_controller(self, name, port, message = ''):
-        jsonmessage = {}
-        start = time.time()
-        current_time = start
-        if not port.isOpen():
-            port.open()
+
+        start_time = time.time()
+
+        self._ensure_port_is_open(port)
+
         ### Your logic goes here ###
         port_lock = self.device_locks[name]
-        port_lock.acquire()
-        if port_lock.locked():
-            print port_lock,'acquired'
-        try:
-            while True:
-                port.write(message)
-                port.flush()
-                print port.readline()
-            jsonmessage = self.read_raw(name, port)
-        except:
-            print 'raised error'
-        port_lock.release()
-        if not port_lock.locked():
-            print port_lock, 'released'
-        last_time = current_time
-        current_time = time.time()
-        print 'method took :', int(current_time - last_time), ' seconds'
+        with port_lock:
+            try:
+                while True:
+                    port.write(message + "\n")
+                    port.flush()
+                    print port.readline()
+                jsonmessage = self.read_raw(name, port)
+            except:
+                print 'raised error'
+
+        print 'method took :', int(time.time() - start_time), ' seconds'
+
         if port.isOpen:
             port.close()
+
         return jsonmessage
 
-    def _build_json(self, message, device_name, delim = ',', key_val_split = '='):
+    def _build_json(self, message, device_name):
         try:
             message = message.rstrip()
             data_thread = {}
+
+            field_separator = None
+            keyval_separator = None
+            in_single_quote = False
+            in_double_quote = False
+            index = 0
+            maxlen = len(message)
+
+            while (index < maxlen) and (
+                  (field_separator is None) or (keyval_separator is None)):
+                c = message[index]
+                if (c == '"') and (not in_single_quote):
+                    in_double_quote = not in_double_quote
+                elif (c == "'") and (not in_double_quote):
+                    in_single_quote = not in_single_quote
+                elif in_single_quote or in_double_quote:
+                    # if in a quoted string, don't check for separators
+                    pass
+                elif (c == ':') or (c == '='):
+                    keyval_separator = c
+                elif (c == ',') or (c == ';') or (c == '\n'):
+                    field_separator = c
+                index += 1
+
             try:
-                key_val_pairs = message.split(delim)
+                key_val_pairs = message.split(field_separator)
                 for pair in key_val_pairs:
-                    key, val = pair.split(key_val_split)
+                    pair_list = pair.split(keyval_separator)
+                    key = pair_list[0].lstrip()
+                    val = pair_list[1].lstrip()
                     data_thread[key] = val
             except:
                 return
@@ -147,8 +169,6 @@ The connection returns in it's open state .
             return json.dumps(data_thread)
         except:
             return
-
-    
 
     def haus_api_put(self):
         pass
@@ -216,7 +236,62 @@ The connection returns in it's open state .
         contents['timestamp'] = time.time()
         return json.dumps(contents)
 
+    def ubuntu_connections(self, group_mode):
+        """ ubuntu (in a vbox) does not alter the /dev dynamically """
+        answer = raw_input("Would you like to set up devices? (y/n)")
+        if (answer.lower()[0] != 'y'):
+            return None
+
+        device_meta_data_field_names = ('device_name', 'device_type', 'username', 'access_key', 'timezone', 'timestamp')
+
+        username = "Charles" or raw_input("What is the account username for all your devices?: ")
+        access_key = "Gust" or raw_input("What is the access key?: ")
+        timezone = "LA" or raw_input("What is your current timezone?: ")
+
+        while raw_input("Would you like to set up a device? (y/n)").startswith('y'):
+            device_name = "LoveMeter" or raw_input("What would you like to call the device?")
+            new_dev = "/dev/ttyS1" or raw_input("What is the path to {}? ".format(device_name))
+            device_type = "monitor" or raw_input("Is {} a 'controller' or a 'monitor'?: ".format(device_name))
+            baud_rate = "9600" or raw_input("The default Baud rate is 9600. Set it now if you like, else hit enter: ")
+            timestamp = time.time()
+
+            device_data = []
+            device_data.append(device_name)
+            device_data.append(device_type)
+            device_data.append(username)
+            device_data.append(access_key)
+            device_data.append(timezone)
+            device_data.append(timestamp)
+
+            metadata = dict(zip(device_meta_data_field_names, device_data))
+            self.device_metadata[device_name] = metadata
+
+            last_port = new_dev
+            try:
+                self.device_locks[device_name] = self.serial_locks[last_port]
+            except KeyError:
+                self.serial_locks[last_port] = Lock()
+                self.device_locks[device_name] = self.serial_locks[last_port]
+            last_device_connected = self.pickup_conn()[-1]
+            if baud_rate != '':
+                try:
+                    last_device_connected.baud_rate = int(baud_rate)
+                except:
+                    raise Exception('Could not set that baud rate, check your input and try again.')
+            if device_type == 'controller':
+                self.controllers[device_name] = last_device_connected
+            elif device_type == 'monitor':
+                self.monitors[device_name] = last_device_connected
+            self.named_connections[device_name] = last_device_connected
+            print("Device {} at {} is registered!".format(device_name, new_dev))
+
+        current_connections = self.named_connections
+        return current_connections
+
     def run_setup(self, group_mode = False):
+        if sys.platform.startswith('linux2'):
+            return self.ubuntu_connections(group_mode)
+
             setup_instructions = """
     There are {} ports available.
     If you would like to run through the device
@@ -281,6 +356,7 @@ The connection returns in it's open state .
                     current_number += 1
                 current_connections = self.named_connections
                 return current_connections
+
 def _serial_ports():
     """Lists serial ports
     :raises EnvironmentError:
@@ -290,7 +366,9 @@ def _serial_ports():
     """
     if sys.platform.startswith('win'):
         ports = ['COM' + str(i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('linux2') or sys.platform.startswith('cygwin'):
+    elif sys.platform.startswith('linux2'):
+        ports = ['/dev/ttyS1']
+    elif sys.platform.startswith('linux')  or sys.platform.startswith('cygwin'):
         ports = glob.glob('/dev/ttyACM*')
     elif sys.platform.startswith('darwin'):
         ### the second glob is for the xbee
